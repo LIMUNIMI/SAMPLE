64ab064113f4b5d746b410b0c9d153858710ffb2
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2021-02-24 14:09:47 +0100

    added unit tests

---
+++ tests/test_sms.py
@@ -0,0 +1,76 @@
+"""Tests related to SMS"""
+import unittest
+from tests import utils
+from sample.sms import sm as sample_sm
+from sample.sms import dsp as sample_dsp
+import numpy as np
+import sys
+import os
+import warnings
+
+
+class TestSMS(utils.RMSEAssertMixin, unittest.TestCase):
+  """Tests related to SMS"""
+  sms_pack_repo = "https://gitlab.com/ChromaticIsobar/sms.git"
+
+  @classmethod
+  def setUpClass(cls):
+    """Download SMS and install it as a package"""
+    if not os.path.isfile("sms/__init__.py"):
+      os.system(
+        "git clone {} sms --recurse-submodules".format(
+          cls.sms_pack_repo
+        )
+      )
+      os.system("cd sms && {} setup.py".format(sys.executable))
+
+  @classmethod
+  def tearDownClass(cls):
+    """Remove SMS"""
+    if not os.environ.get("SMS_REPO_PERSISTENT", False):
+      os.system("rm -rf sms")
+
+  def setUp(self) -> None:
+    """Create a modal-ish sound with three partials for 2 seconds at 44100 Hz"""
+    self.fs = 44100
+    t_tot = 2
+    t = np.linspace(0, t_tot, int(t_tot * self.fs), endpoint=False)
+    f = np.array([440, 650, 690])
+    a = np.array([1, .5, .45])
+    d = np.array([3, 7, 5])
+
+    x = np.squeeze(np.reshape(a, (1, -1)) @ (
+      np.exp(-np.reshape(d, (-1, 1)) * np.reshape(t, (1, -1))) *
+      np.sin(np.reshape(f, (-1, 1)) * 2 * np.pi * np.reshape(t, (1, -1)))
+    ))
+    self.x = x / np.max(np.abs(x))
+    warnings.filterwarnings(
+      "ignore",
+      message="numpy.ufunc size changed"
+    )
+
+  def tearDown(self):
+    """Cleanup after each test"""
+    warnings.resetwarnings()
+
+  def test_input_peaks_at_one(self):
+    """Test that the input audio peaks at 1"""
+    self.assertEqual(np.max(np.abs(self.x)), 1)
+
+  def test_dft(self):
+    """Check that DFT computation is consistent"""
+    from sms.models import dftModel  # pylint: disable=C0415
+
+    sm = sample_sm.SinusoidalModel()
+    sm.w_ = sm.normalized_window
+    for i, x_i in enumerate(sm.time_frames(self.x)):
+      mx, px = sample_dsp.dft(x_i, sm.w_, sm.n)
+      mx_sms, px_sms = dftModel.dftAnal(x_i, sm.w_, sm.n)
+      with self.subTest(frame=i, spectrum="magnitude"):
+        self.assert_almost_equal_rmse(mx, mx_sms)
+      with self.subTest(frame=i, spectrum="phase"):
+        self.assert_almost_equal_rmse(px, px_sms)
+
+
+if __name__ == "__main__":
+  unittest.main()
---
+++ tests/utils.py
@@ -0,0 +1,43 @@
+"""Utilities for test cases"""
+import numpy as np
+import functools
+
+
+def rmse(x: np.ndarray, y: np.ndarray) -> float:
+  """Compute the root-mean-squared error
+
+  Arguments:
+    x (array): First array
+    y (array): Second array
+
+  Returns:
+    (float): The RMSE between x and y"""
+  return np.sqrt(np.mean(np.abs(x - y)**2))
+
+
+class RMSEAssertMixin:
+  """Mixin class for Test cases that require assertions based on the RMSE"""
+  def _assert_rmse(
+    self,
+    x: np.ndarray,
+    y: np.ndarray,
+    almost: bool,
+    *args,
+    **kwargs):
+    """Assert that the RMSE is zero
+
+    Arguments:
+      x (array): First array
+      y (array): Second array
+      almost (bool): If True, then assert that the RMSE is almost zero
+      *args: Positional arguments for :func:`unittest.TestCase.assertEqual`
+        or :func:`unittest.TestCase.assertAlmostEqual`
+      **kwargs: Keyword arguments for :func:`unittest.TestCase.assertEqual`
+        or :func:`unittest.TestCase.assertAlmostEqual`"""
+    return (self.assertAlmostEqual if almost else self.assertEqual)(
+      rmse(x, y), 0,
+      *args, **kwargs,
+    )
+
+  assert_equal_rmse = functools.partialmethod(_assert_rmse, almost=False)
+  assert_almost_equal_rmse = functools.partialmethod(_assert_rmse, almost=True)
