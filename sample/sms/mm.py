77d47a0e4ce476beff062071cb9638acc3cc80b4
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2021-03-22 10:56:14 +0100

    merge strategy option

--- sample/sms/mm.py
+++ sample/sms/mm.py
@@ -2,7 +2,36 @@
 from sample.sms import sm
 from sklearn import linear_model
 import numpy as np
-from typing import Tuple, Optional
+import functools
+from typing import Tuple, Optional, Dict, Callable
+
+
+TractT = Dict[str, np.ndarray]
+
+
+def _track_diff_avg(t: TractT, u: TractT) -> float:
+  """Average frequency difference
+
+  Args:
+    t (track): First track
+    u (track): Second track
+
+  Returns:
+    float: Difference of average frequencies"""
+  return np.abs(np.nanmean(t["freq"]) - np.nanmean(u["freq"]))
+
+
+def _track_diff_single(t: TractT, u: TractT) -> float:
+  """Single frequency difference: difference of the last frequency of
+  :data:`t` and the first difference of :data:`u`
+
+  Args:
+    t (track): First track
+    u (track): Second track
+
+  Returns:
+    float: Frequency difference"""
+  return np.abs(t["freq"][-1] - u["freq"][0])
 
 
 class ModalTracker(sm.SineTracker):
@@ -20,7 +49,11 @@ class ModalTracker(sm.SineTracker):
     freq_dev_slope (float): Slope of frequency deviation threshold
     frequency_bounds (float, float): Minimum and maximum accepted mean frequency
     peak_threshold (float): Minimum peak magnitude in dB for modal tracks
-    reverse (bool): Whether the input audio will be reversed"""
+    reverse (bool): Whether the input audio will be reversed
+    merge_strategy (str): Track merging strategy. Supported strategies are:
+      :data:`"single"` (based on the frequency difference of the tail of older
+      track and head of new track), :data:`"average"` (based on frequency
+      difference of average frequencies)"""
   def __init__(
     self,
     max_n_sines: int,
@@ -30,6 +63,7 @@ class ModalTracker(sm.SineTracker):
     frequency_bounds: Tuple[Optional[float], Optional[float]],
     peak_threshold: float,
     reverse: bool,
+    merge_strategy: str,
   ):
     super().__init__(
       max_n_sines=max_n_sines,
@@ -40,6 +74,7 @@ class ModalTracker(sm.SineTracker):
     self.frequency_bounds = frequency_bounds
     self.peak_threshold = peak_threshold
     self.reverse = reverse
+    self.merge_strategy = merge_strategy
 
   def track_ok(self, track: dict) -> bool:
     """Check if deactivated track is ok to be saved
@@ -70,6 +105,24 @@ class ModalTracker(sm.SineTracker):
 
     return True
 
+  _diff_funcion_dict: Dict[str, Callable] = {
+    "single": _track_diff_single,
+    "average": _track_diff_avg,
+  }
+
+  @property
+  def freq_diff_function(self) -> Callable:
+    """Frequency difference function for current merge strategy"""
+    if self.merge_strategy not in self._diff_funcion_dict:
+      raise KeyError(
+        "merge strategy for object of type '{}'"
+        "should be one of the following: {}".format(
+          self.__class__.__name__,
+          ", ".join(map("'{}'".format, self._diff_funcion_dict.keys()))
+        )
+      )
+    return self._diff_funcion_dict[self.merge_strategy]
+
   def deactivate(self, track_index: int) -> dict:
     """Remove track from list of active tracks and save it in
     :attr:`tracks_` if it meets cleanness criteria
@@ -87,8 +140,8 @@ class ModalTracker(sm.SineTracker):
           lambda u: (u["start_frame"] + u["mag"].size) < t["start_frame"],
           self.tracks_
         ),
-        # absolute difference from last peak's frequency
-        lambda u: np.abs(u["freq"][-1] - t["freq"][0])
+        # absolute difference of the two tracks according to the strategy
+        functools.partial(self.freq_diff_function, u=t)
       )
       if u is not None and df < self.df(t["freq"][0]):
         pad = np.full(
@@ -128,7 +181,11 @@ class ModalModel(sm.SinusoidalModel):
       the attribute :attr:`intermediate_`. Defaults to False
     frequency_bounds (float, float): Minimum and maximum accepted mean frequency
     peak_threshold (float): Minimum peak magnitude (magnitude at time=0) in dB
-      for modal tracks"""
+      for modal tracks
+    merge_strategy (str): Track merging strategy. Supported strategies are:
+      :data:`"single"` (based on the frequency difference of the tail of older
+      track and head of new track), :data:`"average"` (based on frequency
+      difference of average frequencies)"""
   def __init__(
     self,
     fs: int = 44100,
@@ -145,6 +202,7 @@ class ModalModel(sm.SinusoidalModel):
     save_intermediate: bool = False,
     frequency_bounds: Tuple[Optional[float], Optional[float]] = (20, 16000),
     peak_threshold: float = -90,
+    merge_strategy: str = "average",
   ):
     super().__init__(
       fs=fs,
@@ -162,6 +220,7 @@ class ModalModel(sm.SinusoidalModel):
     )
     self.frequency_bounds = frequency_bounds
     self.peak_threshold = peak_threshold
+    self.merge_strategy = merge_strategy
 
   @property
   def sine_tracker_kwargs(self) -> dict:
@@ -171,5 +230,6 @@ class ModalModel(sm.SinusoidalModel):
       frequency_bounds=self.frequency_bounds,
       peak_threshold=self.peak_threshold,
       reverse=self.reverse,
+      merge_strategy=self.merge_strategy,
     ))
     return kwargs
