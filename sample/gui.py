f69b6f3f4cfe6866fb91a49198be4d7996f7da3b
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2021-05-14 17:20:53 +0200

    cli arguments

--- sample/gui.py
+++ sample/gui.py
@@ -1,10 +1,22 @@
 """SAMPLE GUI launcher"""
 from chromatictools import cli
-from sample.widgets import main
+from sample.widgets import main, logging
+import argparse
+import sys
 
 
-@cli.main(__name__)
-def run():
+@cli.main(__name__, *sys.argv[1:])
+def run(*argv):
   """Launch the SAMPLE GUI"""
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument(
+    "-l", "--log-level", dest="log_level", metavar="LEVEL",
+    default="WARNING", type=lambda s: str(s).upper(),
+    help="Set the log level. Default is 'WARNING'",
+  )
+  args, _ = parser.parse_known_args(argv)
+  logging.setLevel(args.log_level)
+  logging.info("Args: %s", args)
+
   main.main().mainloop()
   return 0
--- sample/widgets/__init__.py
+++ sample/widgets/__init__.py
@@ -1,1 +1,12 @@
 """Widgets for the SAMPLE GUI"""
+import logging as _logging
+
+
+_logging.basicConfig(
+  level=_logging.WARNING,
+  format="%(asctime)s %(name)-12s %(levelname)-8s: %(message)s",
+  datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+logging = _logging.getLogger("SAMPLE-GUI")
