d45c05969e847b130bd136f13a53f2b0e8568ba0
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2021-05-15 15:40:48 +0200

    move log setup in main

--- sample/widgets/__init__.py
+++ sample/widgets/__init__.py
@@ -2,11 +2,4 @@
 import logging as _logging
 
 
-_logging.basicConfig(
-  level=_logging.WARNING,
-  format="%(asctime)s %(name)-12s %(levelname)-8s: %(message)s",
-  datefmt="%Y-%m-%d %H:%M:%S",
-)
-
-
 logging = _logging.getLogger("SAMPLE-GUI")
