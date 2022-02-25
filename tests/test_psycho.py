7e9bf71503fa6a6773c7f2b888e93561f6f5a00a
ChromaticIsobar <marcotiraboschi@hotmail.it>
2022-02-25 11:53:16 +0100

    :white_check_mark: Test Mel conversion

--- tests/test_psycho.py
+++ tests/test_psycho.py
@@ -1,33 +1,70 @@
 """Tests for psychoacoustic models"""
 import unittest
+from typing import Callable, Container, Iterable, Optional
+
+import numpy as np
 from chromatictools import unittestmixins
 from sample import psycho
-import numpy as np
 
 
 class TestPsycho(unittestmixins.AssertDoesntRaiseMixin,
                  unittestmixins.RMSEAssertMixin, unittest.TestCase):
   """Tests psychoacoustic models functions"""
 
-  def test_bark(self):
-    f = np.linspace(0, 2e4, 1024)
-    modes = ("unsupported", "zwicker", "traunmuller", "wang")
-    no_forward = ("unsupported",)
-    no_backward = ("zwicker",)
+  def _t3st_coherence(self,
+                      fwd: Callable,
+                      bak: Callable,
+                      modes: Iterable[str],
+                      f=np.linspace(0, 2e4, 1024),
+                      no_fwd: Optional[Container[str]] = None,
+                      no_bak: Optional[Container[str]] = None,
+                      **kwargs):
+    """Test coherence of models
+
+    Args:
+      fwd (callable): Forward conversion function
+      bak (callable): Backward conversion function
+      modes (iterable of str): Modes to test
+      f: Frequency values (in Hertz) to convert
+      no_fwd (container of str): Modes with unsupported forward conversion
+      no_bak (container of str): Modes with unsupported backward conversion
+      kwargs: Keyword arguments for
+        :func:`unittestmixins.RMSEAssertMixin.assert_almost_equal_rmse`"""
+    if no_fwd is None:
+      no_fwd = ()
+    if no_bak is None:
+      no_bak = ()
     for m in modes:
       with self.subTest(mode=m, conversion="forward"):
-        if m in no_forward:
+        if m in no_fwd:
           with self.assertRaises(ValueError):
-            psycho.hz2bark(f, mode=m)
+            fwd(f, mode=m)
         else:
           with self.assert_doesnt_raise():
-            b = psycho.hz2bark(f, mode=m)
+            b = fwd(f, mode=m)
       with self.subTest(mode=m, conversion="backward"):
-        if m in no_forward:
+        if m in no_fwd:
           pass
-        elif m in no_backward:
+        elif m in no_bak:
           with self.assertRaises(ValueError):
-            psycho.bark2hz(b, mode=m)
+            bak(b, mode=m)
         else:
-          f_ = psycho.bark2hz(b, mode=m)
-          self.assert_almost_equal_rmse(f, f_)
+          f_ = bak(b, mode=m)
+          self.assert_almost_equal_rmse(f, f_, **kwargs)
+
+  def test_bark(self):
+    """Test coherence of conversion for Barks"""
+    self._t3st_coherence(
+        fwd=psycho.hz2bark,
+        bak=psycho.bark2hz,
+        modes=("unsupported", "zwicker", "traunmuller", "wang"),
+        no_fwd=("unsupported",),
+        no_bak=("zwicker",),
+    )
+
+  def test_mel(self):
+    """Test coherence of conversion for Mels"""
+    self._t3st_coherence(fwd=psycho.hz2mel,
+                         bak=psycho.mel2hz,
+                         modes=("unsupported", "default", "fant"),
+                         no_fwd=("unsupported",))
