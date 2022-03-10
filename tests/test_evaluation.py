d329328f50cc50b7ba98ec22e2d9c7badc83cd5a
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2022-03-10 15:26:46 +0100

    :white_check_mark: Test both manhattan and euclidean losses

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -23,22 +23,28 @@ class TestMetrics(unittest.TestCase):
                                                          self.x,
                                                          njobs=6))
 
-  def test_multiscale_coherence(self, dropout=(0, 0.20, 0.40, 0.60)):
+  def test_multiscale_coherence(self,
+                                dropout=(0, 0.20, 0.40, 0.60),
+                                powers=(1, 2)):
     """Test coherence of multiscale spectral loss"""
     dropout = sorted(dropout)
-    prev = prev_d = None
     np.random.seed(42)
     r = np.random.rand(np.size(self.x))
     with mp.Pool(processes=6) as pool:
-      for d in dropout:
-        y = np.greater(r, d).astype(float) * self.x
-        curr = metrics.multiscale_spectral_loss(self.x, y, pool=pool)
-        if d == 0:
-          with self.subTest(test="zero"):
-            self.assertEqual(0, curr)
-        else:
-          if prev is not None:
-            with self.subTest(test="order", prev=prev_d, curr=d):
-              self.assertLess(prev, curr)
-        prev = curr
-        prev_d = d
+      for p in powers:
+        prev = prev_d = None
+        for d in dropout:
+          y = np.greater(r, d).astype(float) * self.x
+          curr = metrics.multiscale_spectral_loss(self.x,
+                                                  y,
+                                                  norm_p=p,
+                                                  pool=pool)
+          if d == 0:
+            with self.subTest(power=p, test="zero"):
+              self.assertEqual(0, curr)
+          else:
+            if prev is not None:
+              with self.subTest(power=p, test="order", prev=prev_d, curr=d):
+                self.assertLess(prev, curr)
+          prev = curr
+          prev_d = d
