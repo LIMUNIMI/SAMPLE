e65299c5261a857f989dea378fdf302d31fa4fd1
ChromaticIsobar <marcotiraboschi@hotmail.it>
2022-11-18 15:07:28 +0100

    :memo: Reformat docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,32 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
 
+This file only contains a selection of the most common options. For a full
+list see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html"""
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath("../.."))
 
+import importlib
+import os
+import subprocess
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 
 project = "Spectral Analysis for Modal Parameter Linear Estimate"
-copyright = "2021-2022, Marco Tiraboschi"
+copyright = "2021-2022, Marco Tiraboschi"  # pylint: disable=W0622
 author = "Marco Tiraboschi"
 
-from sample import __version__ as version
+if os.environ.get("SAMPLE_SPHINX_VERSION_IS_SHA", False):
+  version = subprocess.check_output("git rev-parse HEAD",
+                                    shell=True).decode("utf-8")
+else:
+  version = importlib.import_module("sample").__version__
 release = version
 
 # -- General configuration ---------------------------------------------------
@@ -49,7 +54,6 @@ templates_path = ["_templates"]
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 # The theme to use for HTML and HTML Help pages.  See the documentation for
