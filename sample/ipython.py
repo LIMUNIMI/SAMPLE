e08fc79d53ee2424c1a5e0b2ab7b6623b0fc10c2
ChromaticIsobar <marcotiraboschi@hotmail.it>
2022-12-16 18:20:46 +0100

    :sparkles: Nested collapsible HTML menu to inspect model arguments

--- sample/ipython.py
+++ sample/ipython.py
@@ -1,9 +1,12 @@
 """Utilities for IPython"""
 import contextlib
+import functools
+import itertools
 import json
 import os
+import re
 import time
-from typing import Optional
+from typing import Any, Dict, Optional
 
 import numpy as np
 from IPython import display as ipd
@@ -16,10 +19,7 @@ class WebAudio(ipd.HTML):
     x (array): Array of audio samples with shape :data:`(channels, samples)`
       or :data:`(samples,)`
     rate (int): Sample rate
-    label (str): Play button label
-
-  Returns:
-    IPython.display.HTML: HTML element with WebAudio content"""
+    label (str): Play button label"""
 
   def __init__(self, x, rate: int, label: str = "Play"):
     with open(os.path.join(os.path.dirname(__file__), "web_audio.html"),
@@ -30,6 +30,93 @@ class WebAudio(ipd.HTML):
                                   -1)).tolist()),
                                        rate=rate,
                                        label=label))
+
+
+class CollapsibleModelParams(ipd.HTML):
+  """Display model parameters in a nested collapsible HTML
+
+  Args:
+    model: Model of which the parameters should be displayed"""
+
+  # Regex for sphinx commands
+  _SPHINXCLASS_REGEX = re.compile(r"\:\w+\:`.+`")
+  _SINGLE_WS_REGEX = re.compile(r"\s")
+
+  @staticmethod
+  def _unwrap_sphinx_inner(m: re.Match):
+    return m.group(0).split("`", 2)[1]
+
+  @staticmethod
+  def _unwrap_sphinx(s: str):
+    """Remove sphinx commands"""
+    return CollapsibleModelParams._SPHINXCLASS_REGEX.sub(
+        CollapsibleModelParams._unwrap_sphinx_inner, s)
+
+  @staticmethod
+  def _parse_docs(docs: Optional[str] = None):
+    """Extract a list of argument descriptions from a docstring"""
+    if docs is None:
+      return []
+    docs = CollapsibleModelParams._unwrap_sphinx(docs)
+    docs = docs.split("Args:", 1)
+    if len(docs) < 2:
+      return []
+    docs = filter(lambda s: s, docs[1].split("\n"))
+    docs_ = []
+    s = []
+    indent = np.inf
+    for d in docs:
+      current_indent = len(
+          list(
+              itertools.takewhile(
+                  CollapsibleModelParams._SINGLE_WS_REGEX.fullmatch, d)))
+      if current_indent <= indent:
+        indent = current_indent
+        if s:
+          docs_.append(s)
+          s = []
+      s.append(d)
+    docs_.append(s)
+    return list(map(" ".join, map(functools.partial(map, str.strip), docs_)))
+
+  class _ModelWrapper:
+    """Meant to wrap the input of :class:`CollapsibleModelParams`"""
+
+    def __init__(self, m):
+      self.m = m
+
+    def get_params(self) -> Dict[str, Any]:
+      return {type(self.m).__name__: self.m}
+
+  @staticmethod
+  def _html_model(model) -> str:
+    """Get the HTML body for a model and its arguments"""
+    lines = []
+    docs = CollapsibleModelParams._parse_docs(model.__doc__)
+    for k, v in filter(lambda t: len(t[0].split("__", 1)) == 1,
+                       model.get_params().items()):
+      is_model = hasattr(v, "get_params")
+      if is_model:
+        lines.append("<button type=\"button\" class=\"collapsible\">")
+      else:
+        lines.append("<div class=\"noncollapsible\">")
+      rk = re.compile("^" + k + r"\W")
+      d = list(filter(rk.match, docs))
+      lines.extend(d if d else [k])
+      if is_model:
+        lines.append("</button>")
+        lines.append("<div class=\"content\">")
+        lines.append(CollapsibleModelParams._html_model(v))
+      lines.append("</div>")
+    return "\n".join(lines)
+
+  def __init__(self, model):
+    with open(os.path.join(os.path.dirname(__file__),
+                           "collapsible_params.html"),
+              mode="r",
+              encoding="utf-8") as f:
+      super().__init__(
+          f.read().format(body=self._html_model(self._ModelWrapper(model))))
 
 
 @contextlib.contextmanager
